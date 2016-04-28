184509c4c0c145af554859452555a0f0b6221540
Cade Nichols <cade@cadenichols.com>
2016-04-28 14:46:16 -0700

    modularization

---
+++ public/js/controllers.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var app = angular.module('todoApp');
+
+// controllers.js
+// all controllers
+
+app.controller('mainCtrl', function() {
+  console.log('mainCtrl!');
+});
---
+++ public/js/services.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var app = angular.module('todoApp');
+
+// services.js
+// all services and factories
+
